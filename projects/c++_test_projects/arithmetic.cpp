a6ab98945df9a96d54b8c51fe9c3e262280e27f9
Сергей Д. Толузаров <stoluzarov@ups-volt.ru>
2026-02-17 18:46:15 +0500

    asds

---
+++ projects/c++_test_projects/arithmetic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+// int main()
+// {
+//     int meters;
+//     int kilometers = 1000;
+//     cin>>meters;
+//     cout<<meters*kilometers;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int radius;
+//     const double pi = 3.14;
+//     cin >> radius;
+//     cout << pi * radius * radius;
+// }
